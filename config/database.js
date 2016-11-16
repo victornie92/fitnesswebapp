0367ff5c3b014dac13906eb779d1f357c4ad2748
Victor Nielsen <victor.nielsen@hotmail.com>
2016-11-16 09:27:51 +0100

    added the connection to heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/FitnessDB'
+    //'url' : 'mongodb://localhost/FitnessDB'
+    'url' : 'mongodb://fitnesswebappdb:fitnesswebappdb@ds013545.mlab.com:13545/heroku_rhnsjtgw'
 };
--- server.js
+++ server.js
@@ -10,10 +10,63 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var configDB = require('./config/database.js');
+var gracefulShutdown;
+//var dbURI = require('./config/database.js');
+
+var dbURI = 'mongodb://fitnesswebappdb:fitnesswebappdb@ds013545.mlab.com:13545/heroku_rhnsjtgw'
+
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+mongoose.connect(dbURI); // connect to our database
+
+// Monitoring for successful connection through Mongoose
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+// Checking for connection error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+// Checking for disconnection event
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+// Close Mongoose connection, passing through an anonymous function to run when closed
+// Define function to accept message and callback function
+
+gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+    // Output message and call callback when Mongoose connection is closed
+  });
+};
+// Listen for SIGUSR2, which is what nodemon uses
+process.once('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+      // Send message to graceful- Shutdown and callback to kill process, emitting SIGUSR2 again
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+// Listen for SIGINT emitted on application termination
+process.on('SIGINT', function () {
+  gracefulShutdown('app termination', function () {
+      //Send message to gracefulShutdown and callback to exit Node process
+    process.exit(0);
+  });
+});
+
+// Listen for SIGTERM emitted when Heroku shuts down process
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function () {
+      //Send message to gracefulShutdown and callback to exit Node process
+    process.exit(0);
+  });
+});
 
 require('./config/passport')(passport); // pass passport for configuration
 
